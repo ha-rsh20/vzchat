c94f62f36acd036190b0b4256f7622584d91a735
ha-rsh20 <20it033@charusat.edu.in>
2023-10-17 01:26:57 +0530

    some changes

--- vzchat-back/back.js
+++ vzchat-back/back.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "http://localhost:3000"],
+    origin: ["https://vzchat.netlify.app/", "https://vzchat.netlify.app"],
   })
 );
 
